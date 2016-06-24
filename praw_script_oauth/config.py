7f334061e3cc95fbe4c5993a730fa8d6ee7c835e
Enigma <theenigmablade@gmail.com>
2016-06-24 19:44:29 -0400

    Added handling of invalid token file content

--- praw_script_oauth/config.py
+++ praw_script_oauth/config.py
@@ -7,11 +7,12 @@ def read_config(config_file, config_dir="."):
 			token = f.readline().strip()
 			time = int(f.readline().strip())
 			return token, time
-	except FileNotFoundError:
+	except (FileNotFoundError, ValueError):
 		return None, 0
 
 def write_config(token, time, config_file, config_dir="."):
-	path = os.path.join(config_dir, config_file)
-	with open(path, "w") as f:
-		f.write(str(token)+"\n")
-		f.write(str(time))
+	if token is not None:
+		path = os.path.join(config_dir, config_file)
+		with open(path, "w") as f:
+			f.write(str(token)+"\n")
+			f.write(str(time))
